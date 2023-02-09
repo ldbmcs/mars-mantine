23c7bc62eb932160f8420b0340ff437b1ec0307e
dengchaosheng <ldbmcs@gmail.com>
2023-02-10 07:07:55 +0800

    Improve site components and features
    - Add `eslint-plugin-prettier` to `package.json`
    - Add HeroHeader, SiteHeader, ContactUs, Footer, Features, and Faq components
    - Add data to FooterLinksProps and SiteHeader links
    - Add styling for components and global theme object
    - Add form with fields for name, email, subject, and message
    - Add MantineLogo and social media icons
    - Add accordion with five questions and answers
    
    [package.json]
    - Add `eslint-plugin-prettier` to `package.json`
    [src/components/business/HeroHeader/index.tsx]
    - Add HeroHeader component
    - Create styles for HeroHeader component
    - Add background image and overlay to HeroHeader component
    - Adjust font size and line height of HeroHeader component title based on screen size
    - Adjust HeroHeader component description max width based on screen size
    - Add HeroHeader component control button
    [yarn.lock]
    - Add `eslint-plugin-prettier` package
    - Add `prettier-linter-helpers` package
    - Update `fast-diff` package
    - Update `fast-glob` package
    - Update `eslint-plugin-react-hooks` package
    - Update `fast-deep-equal` package
    - Update `prelude-ls` package
    - Update `prettier` package
    [src/components/business/SiteHeader/index.tsx]
    - Add SiteHeader component
    - Add useStyles hook for styling
    - Add useDisclosure hook for toggling dropdown
    - Add links to the header
    - Add active class to the links when active
    - Add responsive design for mobile view
    [src/pages/index.tsx]
    - Add React import
    - Add components for HeroHeader, Features, Faq, ContactUs, and Footer
    - Add data to FooterLinksProps and SiteHeader links
    - Replace "Home" with a Stack containing the components
    [src/components/business/ContactUs/index.tsx]
    - Add a ContactUs component
    - Add TextInput, Textarea, Button, Group, and SimpleGrid components
    - Add style rules for the ContactUs component
    - Add a form with fields for name, email, subject, and message
    - Add a submit button
    [src/components/business/Footers/index.tsx]
    - Add a new file for the Footer component
    - Add styling for the footer, logo, description, groups, wrapper, link, title and afterFooter
    - Add a Footer component with data props and links
    - Add MantineLogo and social media icons
    [src/components/business/Features/index.tsx]
    - Add a new file for the Mantine features
    - Add a free and open source feature
    - Add a TypeScript based feature
    - Add a feature for no annoying focus ring
    - Add a feature for customizing colors, spacing, shadows, fonts and many other settings with global theme object
    [src/components/business/Faq/index.tsx]
    - Add a FAQ page with an accordion
    - Include a title, image, and five questions with answers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,108 @@
+import React from 'react'
+import { HeroHeader } from 'components/business/HeroHeader'
+import { Stack } from '@mantine/core'
+import { Features } from 'components/business/Features'
+import { Faq } from 'components/business/Faq'
+import { ContactUs } from 'components/business/ContactUs'
+import { Footer, FooterLinksProps } from 'components/business/Footers'
+import { SiteHeader } from 'components/business/SiteHeader'
+
 export default function HomePage() {
+  const footerData = {
+    data: [
+      {
+        title: 'About',
+        links: [
+          {
+            label: 'Features',
+            link: '#',
+          },
+          {
+            label: 'Pricing',
+            link: '#',
+          },
+          {
+            label: 'Support',
+            link: '#',
+          },
+          {
+            label: 'Forums',
+            link: '#',
+          },
+        ],
+      },
+      {
+        title: 'Project',
+        links: [
+          {
+            label: 'Contribute',
+            link: '#',
+          },
+          {
+            label: 'Media assets',
+            link: '#',
+          },
+          {
+            label: 'Changelog',
+            link: '#',
+          },
+          {
+            label: 'Releases',
+            link: '#',
+          },
+        ],
+      },
+      {
+        title: 'Community',
+        links: [
+          {
+            label: 'Join Discord',
+            link: '#',
+          },
+          {
+            label: 'Follow on Twitter',
+            link: '#',
+          },
+          {
+            label: 'Email newsletter',
+            link: '#',
+          },
+          {
+            label: 'GitHub discussions',
+            link: '#',
+          },
+        ],
+      },
+    ],
+  } as FooterLinksProps
+  const siteHeaderLinks = {
+    links: [
+      {
+        link: '/about',
+        label: 'Features',
+      },
+      {
+        link: '/pricing',
+        label: 'Pricing',
+      },
+      {
+        link: '/learn',
+        label: 'Learn',
+      },
+      {
+        link: '/community',
+        label: 'Community',
+      },
+    ],
+  }
   return (
-    <>
-      <div>Home</div>
-    </>
+    <Stack>
+      <SiteHeader links={siteHeaderLinks.links} />
+      <HeroHeader />
+      <Features />
+      <Faq />
+      <ContactUs />
+      <Footer data={footerData.data} />
+    </Stack>
   )
 }
