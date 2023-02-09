6209e9b7cb4e86c7119d1d146f4256c17899fa7b
dengchaosheng <ldbmcs@gmail.com>
2023-02-09 18:21:27 +0800

    Upgrade Mantine packages and UI components
    - Update the Mantine logo
    - Upgrade packages to version `5.10.3`
    - Adjust width of `Group` element to `100%`
    - Remove link from logo and `Home` route from `AvatarMenu` component
    
    [src/components/business/ApplicationLayout/Header/index.tsx]
    - Remove the link from the logo
    - Change the width of the `Group` element to `100%`
    - Remove the `Home` route from the `AvatarMenu` component
    [src/components/ui/Logo/index.tsx]
    - Replace the logo image with the Mantine logo
    - Update the logo size from `80x32` to `28x28`
    - Update the link to the home page
    [package.json]
    - Upgrade `@mantine/core`, `@mantine/ds`, `@mantine/hooks`, and `@mantine/prism` to version `5.10.3`

--- src/components/business/ApplicationLayout/Header/index.tsx
+++ src/components/business/ApplicationLayout/Header/index.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Group, Header } from '@mantine/core'
 
 import AvatarMenu from './AvatarMenu'
-import { Home } from 'config/constants/routerPath'
 import { Logo } from 'components/ui'
 
 const MarsHeader = () => (
   <Header height={52} px="lg" display="flex">
-    <Group position="apart" w={'100%'}>
+    <Group position="apart" w="100%">
       <Group position="left" spacing="lg">
-        <Logo href={Home} />
+        <Logo />
       </Group>
       <Group spacing="md">
         <Group spacing="sm">
--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,39 +1,12 @@
 import React, { FC } from 'react'
-import { Image, useMantineTheme } from '@mantine/core'
 import { Home } from 'config/constants/routerPath'
-import { ColorSchemeType } from './type'
 import { AnchorLink } from '..'
+import { MantineLogo } from '@mantine/ds'
 
-interface LogoProps extends LogoImageProps {
-  href?: string
-}
-
-interface LogoImageProps {
-  colorScheme?: ColorSchemeType
-}
-
-const LogoImage: FC<LogoImageProps> = ({ colorScheme }) => {
-  const { colorScheme: defaultColorScheme } = useMantineTheme()
-  return (
-    <Image
-      src="https://static-prod.howxm.com/_next/static/media/logo-dark.9bdb8b86.png"
-      alt="xm-logo"
-      width={80}
-      height={32}
-    />
-  )
-}
-
-const Logo: FC<LogoProps> = ({ href = '', colorScheme }) => (
-  <>
-    {href ? (
-      <AnchorLink href={Home}>
-        <LogoImage colorScheme={colorScheme} />
-      </AnchorLink>
-    ) : (
-      <LogoImage colorScheme={colorScheme} />
-    )}
-  </>
+const Logo: FC = () => (
+  <AnchorLink href={Home}>
+    <MantineLogo size={28} />
+  </AnchorLink>
 )
 
 export default Logo
