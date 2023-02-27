6503e9c83a6ea96a67e253ee632dfa9e2e65bf75
dengchaosheng <ldbmcs@gmail.com>
2023-02-28 07:31:51 +0800

    Add input components and props
    - Add new input components: Autocomplete, Center, ColorInput, MultiSelect, NativeSelect, PasswordInput, Select, and TextInput
    - Add additional props to TextInput component: placeholder, label, description, defaultValue, and error
    - Add data prop to Select, MultiSelect, and Autocomplete components
    - Add searchable and nothingFound props to Select component
    - Add withAsterisk prop to TextInput component
    
    [src/pages/home/inputs/index.tsx]
    - Add Autocomplete, Center, ColorInput, MultiSelect, NativeSelect, PasswordInput, Select, and TextInput components
    - Placeholder, label, description, defaultValue, and error props added to TextInput component
    - Add data prop to Select, MultiSelect, and Autocomplete components
    - Add searchable prop to Select component
    - Add nothingFound prop to Select component
    - Add withAsterisk prop to TextInput component

--- src/pages/home/inputs/index.tsx
+++ src/pages/home/inputs/index.tsx
@@ -1,9 +1,70 @@
 import React, { ReactElement } from 'react'
-import { Container } from '@mantine/core'
+import {
+  Autocomplete,
+  Center,
+  ColorInput,
+  MultiSelect,
+  NativeSelect,
+  PasswordInput,
+  Select,
+  SimpleGrid,
+  TextInput,
+} from '@mantine/core'
 import MarsLayout from 'components/business/ApplicationLayout'
 import { NextPageWithLayout } from 'types/nextCustomType'
 
-const Inputs: NextPageWithLayout = () => <Container size={700}>Inputs</Container>
+const Inputs: NextPageWithLayout = () => (
+  <Center>
+    <SimpleGrid cols={2} w="960px">
+      <TextInput placeholder="Your name" label="Text input" withAsterisk />
+      <PasswordInput placeholder="Password input" label="Password" withAsterisk />
+      <ColorInput placeholder="Pick color" label="Color input" defaultValue="#C5D899" />
+      <NativeSelect data={['React', 'Vue', 'Angular', 'Svelte']} label="Native select" withAsterisk />
+      <Select
+        label="Custom select"
+        defaultValue="react"
+        data={[
+          { value: 'react', label: 'React' },
+          { value: 'ng', label: 'Angular' },
+          { value: 'svelte', label: 'Svelte' },
+          { value: 'vue', label: 'Vue' },
+        ]}
+      />
+      <Select
+        label="Searchable select"
+        placeholder="Search me"
+        searchable
+        nothingFound="No options"
+        data={['React', 'Angular', 'Svelte', 'Vue']}
+      />
+      <MultiSelect
+        data={[
+          { value: 'react', label: 'React' },
+          { value: 'ng', label: 'Angular' },
+          { value: 'svelte', label: 'Svelte' },
+          { value: 'vue', label: 'Vue' },
+          { value: 'riot', label: 'Riot' },
+          { value: 'next', label: 'Next.js' },
+          { value: 'blitz', label: 'Blitz.js' },
+        ]}
+        label="Custom multi select"
+        placeholder="Pick all you want"
+      />
+      <Autocomplete
+        label="Autocomplete"
+        placeholder="Enter anything with your suggestions"
+        data={['React', 'Angular', 'Svelte', 'Vue']}
+      />
+      <TextInput
+        placeholder="Additional info is always useful"
+        description="Every input can have description..."
+        label="With description"
+        withAsterisk
+      />
+      <TextInput label="With error" error="... and error" defaultValue="Oh-oh" />
+    </SimpleGrid>
+  </Center>
+)
 
 Inputs.getLayout = function getLayout(page: ReactElement) {
   return <MarsLayout>{page}</MarsLayout>
