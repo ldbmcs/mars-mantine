d2b5b7e6feab4c29da7d1d9bd04e629dac2f7ecf
dengchaosheng <ldbmcs@gmail.com>
2023-03-01 10:28:49 +0800

    Add dark mode and active link support to Nav component
    - Add dark mode support to the Nav component
    - Add styling for the active link
    - Change icon imports to default imports
    - Add onClick handler to the link to set the active link
    
    [src/components/business/ApplicationLayout/Nav.tsx]
    - Add support for dark mode in the Nav component
    - Add a useState hook to keep track of the active link
    - Add styling for the active link
    - Change the icon imports from named imports to default imports
    - Add an onClick handler to the link to set the active link

--- src/components/business/ApplicationLayout/Nav.tsx
+++ src/components/business/ApplicationLayout/Nav.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react'
-import { Navbar, NavLink } from '@mantine/core'
+import React, { FC, useState } from 'react'
+import { createStyles, Navbar } from '@mantine/core'
 import {
   IconBuildingCarousel,
   IconContainer,
@@ -18,32 +18,87 @@ import {
   Overlays,
   RichTextEditor,
 } from 'config/constants/routerPath'
-import { useRouter } from 'next/router'
+
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon')
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 1.5,
+      borderBottom: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+    },
+
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
+
+    linkIcon: {
+      ref: icon,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        [`& .${icon}`]: {
+          color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+        },
+      },
+    },
+  }
+})
 
 const data = [
-  { pathname: Inputs, label: 'Inputs', icon: <IconInputSearch /> },
-  { pathname: DatePickers, label: 'DatePickers', icon: <IconTimeline /> },
-  { pathname: Navigation, label: 'Navigation', icon: <IconNavigation /> },
-  { pathname: Overlays, label: 'Overlays', icon: <IconOverline /> },
-  { pathname: Content, label: 'Content', icon: <IconContainer /> },
-  { pathname: RichTextEditor, label: 'RichTextEditor', icon: <IconEdit /> },
-  { pathname: Carousel, label: 'Carousel', icon: <IconBuildingCarousel /> },
+  { pathname: Inputs, label: 'Inputs', icon: IconInputSearch },
+  { pathname: DatePickers, label: 'DatePickers', icon: IconTimeline },
+  { pathname: Navigation, label: 'Navigation', icon: IconNavigation },
+  { pathname: Overlays, label: 'Overlays', icon: IconOverline },
+  { pathname: Content, label: 'Content', icon: IconContainer },
+  { pathname: RichTextEditor, label: 'RichTextEditor', icon: IconEdit },
+  { pathname: Carousel, label: 'Carousel', icon: IconBuildingCarousel },
 ]
 const Nav: FC = () => {
-  const { pathname: currentPath } = useRouter()
-
-  const links = data.map((item) => {
-    return (
-      <NavLink
-        key={item.label}
-        label={item.label}
-        active={currentPath === item.pathname}
-        icon={item.icon}
-        component="a"
-        href={item.pathname}
-      />
-    )
-  })
+  const { classes, cx } = useStyles()
+  const [active, setActive] = useState('Inputs')
+  const links = data.map((item) => (
+    <a
+      className={cx(classes.link, { [classes.linkActive]: item.label === active })}
+      href={item.pathname}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault()
+        setActive(item.label)
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ))
 
   return (
     <Navbar width={{ sm: 300 }} p="md">
