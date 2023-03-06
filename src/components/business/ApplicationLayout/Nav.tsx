b10920f839adf11df763a28dc1ab1e9f4d78e0a6
dengchaosheng <ldbmcs@gmail.com>
2023-03-06 21:47:51 +0800

    Update Navbar styles for rems and colors
    - Update styles of Navbar to use rems instead of pixels
    - Change margins, padding, font size and colors for header and footer
    - Change active link background color and color of icons in links
    
    [src/components/business/ApplicationLayout/Nav.tsx]
    - Update Navbar styles to use rems instead of pixels
    - Change the bottom margin of the header and footer
    - Change the border bottom and top of the header and footer
    - Change the padding and font size of the links
    - Change the active link background color and color
    - Change the color of the icons in the links

--- src/components/business/ApplicationLayout/Nav.tsx
+++ src/components/business/ApplicationLayout/Nav.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react'
-import { createStyles, Navbar, getStylesRef } from '@mantine/core'
+import { createStyles, Navbar, getStylesRef, rem } from '@mantine/core'
 import {
   IconBuildingCarousel,
   IconContainer,
@@ -19,59 +19,56 @@ import {
   RichTextEditor,
 } from 'config/constants/routerPath'
 
-const useStyles = createStyles((theme, _params) => {
-  const icon = getStylesRef('icon')
-  return {
-    header: {
-      paddingBottom: theme.spacing.md,
-      marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-      borderBottom: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
-    },
+const useStyles = createStyles((theme, _params) => ({
+  header: {
+    paddingBottom: theme.spacing.md,
+    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
+    borderBottom: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+  },
 
-    footer: {
-      paddingTop: theme.spacing.md,
-      marginTop: theme.spacing.md,
-      borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
-    },
+  footer: {
+    paddingTop: theme.spacing.md,
+    marginTop: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+  },
 
-    link: {
-      ...theme.fn.focusStyles(),
-      display: 'flex',
-      alignItems: 'center',
-      textDecoration: 'none',
-      fontSize: theme.fontSizes.sm,
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
-      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-      borderRadius: theme.radius.sm,
-      fontWeight: 500,
+  link: {
+    ...theme.fn.focusStyles(),
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    fontSize: theme.fontSizes.sm,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
 
-      '&:hover': {
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+      [`& .${getStylesRef('icon')}`]: {
         color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-
-        [`& .${icon}`]: {
-          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        },
       },
     },
+  },
 
-    linkIcon: {
-      ref: icon,
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
-      marginRight: theme.spacing.sm,
-    },
+  linkIcon: {
+    ref: getStylesRef('icon'),
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+    marginRight: theme.spacing.sm,
+  },
 
-    linkActive: {
-      '&, &:hover': {
-        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      [`& .${getStylesRef('icon')}`]: {
         color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-        [`& .${icon}`]: {
-          color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-        },
       },
     },
-  }
-})
+  },
+}))
 
 const data = [
   { pathname: Inputs, label: 'Inputs', icon: IconInputSearch },
